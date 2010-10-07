25f25d73e7efb1e5019282deb4bd3428fb642e04
he9lin <he9lin@gmail.com>
2010-10-06 20:48:27 -0700

    temp change

--- lib/generators/comment/comment_generator.rb
+++ lib/generators/comment/comment_generator.rb
@@ -1,18 +1,25 @@
 require 'rails/generators/migration'
+ 
+module ActsAsCommentable
+  class CommentGenerator < Rails::Generators::Base
+    include Rails::Generators::Migration                 
+    
+    desc "Generates migration for Comment model" 
 
-class CommentGenerator < Rails::Generators::Base
-  include Rails::Generators::Migration
+    def self.source_root
+      File.join File.dirname(__FILE__), 'templates'
+    end
 
-  def self.source_root
-    @_acts_as_commentable_source_root ||= File.expand_path("../templates", __FILE__)
-  end
+    def self.next_migration_number(path)
+      Time.now.utc.strftime("%Y%m%d%H%M%S")
+    end
 
-  def self.next_migration_number(path)
-    Time.now.utc.strftime("%Y%m%d%H%M%S")
-  end
+    def create_migration_file
+      migration_template 'migration.rb', 'db/migrate/acts_as_commentable_migration'
+    end
 
-  def create_model_file
-    template "comment.rb", "app/models/comment.rb"
-    migration_template "create_comments.rb", "db/migrate/create_comments.rb"
+    # def create_model_file
+    #   template "comment.rb", "app/models/comment.rb"
+    # end  
   end
 end
